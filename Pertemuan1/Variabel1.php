ace95bb7cd9eea9ef95e08a5cbe7c16f33ea8ad1
Ella Aisia <162269561+ellaaisia1@users.noreply.github.com>
2024-10-15 16:46:30 +0700

    Add files via upload

---
+++ Pertemuan1/Variabel1.php
@@ -0,0 +1,14 @@
+<?php
+ echo $nama = "ELLA AISIA";
+ echo $umur = 19;
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contoh Variabel PHP</title>
+    </head>
+    <body>
+        <h1>Halo ,<?php echo $nama; ?>!</h1>
+        <p>Umur Anda adalah <?php echo $umur;?>
+    </body>
+    </html>
