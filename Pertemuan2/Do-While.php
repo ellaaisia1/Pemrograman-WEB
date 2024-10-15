aea3d1e05faf2588772a514f08b47178b263df94
Ella Aisia <162269561+ellaaisia1@users.noreply.github.com>
2024-10-15 16:57:33 +0700

    Add files via upload

---
+++ Pertemuan2/Do-While.php
@@ -0,0 +1,7 @@
+<?php
+    $counter = 1; 
+do { 
+    echo "Iterasi ke-$counter <br>"; 
+    $counter++; 
+} while ($counter <= 5); 
+?>
---
+++ Pertemuan2/while.php
@@ -0,0 +1,7 @@
+<?php
+$counter = 1; 
+    while ($counter <= 5) { 
+    echo "Iterasi ke-$counter <br>"; 
+    $counter++; 
+} 
+?>
